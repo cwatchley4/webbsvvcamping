69cfcba1e42948bba66c71a667a0347b6f8d820e
cwatchley4 <cwatchley4@gmail.com>
2024-06-10 21:57:27 -0500

    Added sticky header functionality and changed header styles

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
 "use strict";
 
-const lengthOfStay = document.querySelector(".length-of-stay");
-const price = document.querySelector(".price");
-const submit = document.querySelector(".submit");
+/* ALL PAGES */
 
-submit.addEventListener("click", function () {
-  if (!lengthOfStay)
-    price.textContent = "Please enter a number for length of stay.";
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropDownMenu = document.querySelector(".dropdown-menu");
+const header = document.querySelector(".header");
+const hero = document.querySelector(".hero");
+const pageHero = document.querySelector(".page-hero");
+
+// Nav Dropdown
+
+const isOpen = dropDownMenu.classList.contains("open");
+
+const toggleMobileNavIcon = function () {
+  const isOpen = dropDownMenu.classList.contains("open");
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
+
+const toggleMobileNav = function () {
+  dropDownMenu.classList.toggle("open");
+  toggleMobileNavIcon();
+};
+
+const closeMobileNav = function () {
+  dropDownMenu.classList.remove("open");
+  toggleMobileNavIcon();
+};
+
+toggleBtn.addEventListener("click", toggleMobileNav);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") closeMobileNav();
 });
+document.querySelector("main").addEventListener("click", closeMobileNav);
+
+// Sticky Nav
+
+const headerHeight = header.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (entry.isIntersecting) {
+    header.classList.remove("sticky");
+  } else {
+    header.classList.add("sticky");
+  }
+};
+
+const heroObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerHeight}px`,
+});
+
+if (hero) heroObserver.observe(hero);
+if (pageHero) heroObserver.observe(pageHero);
